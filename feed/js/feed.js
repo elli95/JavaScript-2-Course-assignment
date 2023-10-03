5141f5bf9ae92bb9226488c070736b948dc2e24e
Elise Marie Hogsnes <39590842+elli95@users.noreply.github.com>
2023-10-03 19:55:44 +0200

    js for post on feed page

---
+++ feed/js/feed.js
@@ -0,0 +1,42 @@
+import { postContent } from "../../src/js/modules/source.mjs";
+
+const API_BASE_URL = "https://api.noroff.dev";
+
+const profileUrl = `${API_BASE_URL}/api/v1/social/posts`;
+
+async function postData() {
+  try {
+    const accessToken = localStorage.getItem("accessToken");
+
+    const fetchPostData = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    };
+
+    const response = await fetch(profileUrl, fetchPostData);
+    // console.log("response", response);
+    const data = await response.json();
+    console.log("data", data);
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function loadPosts(data) {
+  try {
+    const postInfo = await postData(data);
+    // console.log(postInfo);
+    Object.values(postInfo).forEach(function (post) {
+      postContent(post);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+loadPosts();
