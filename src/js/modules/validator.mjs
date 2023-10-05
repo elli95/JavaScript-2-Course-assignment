ab7c1c8eaf8490fc11ccb3f481d20c41554e175b
Elise Marie Hogsnes <39590842+elli95@users.noreply.github.com>
2023-10-05 18:48:22 +0200

    Form validations

--- src/js/modules/validator.mjs
+++ src/js/modules/validator.mjs
@@ -1,22 +1,17 @@
-
 //Reference: Email address validation pattern from https://regexr.com/3e48o
 //Reference: function structure from https://content.noroff.dev/javascript-1/form-validation.html#regular-expressions
 function userEmailValidation(userEmail) {
-  const emailPattern = (/^[\w-\.]+@noroff.no|^[\w-\.]+@stud.noroff.no/);
+  const emailPattern = /^[\w-\.]+@noroff.no|^[\w-\.]+@stud.noroff.no/;
   const emailPatternCheck = emailPattern.test(userEmail);
   return emailPatternCheck;
 }
-  
-function valueLength(value, inputLength){
-  if (value.trim().length >= inputLength){
+
+function valueLength(value, inputLength) {
+  if (value.trim().length >= inputLength) {
     return true;
-  } 
-  else {
+  } else {
     return false;
   }
 }
 
-export {
-    userEmailValidation,
-    valueLength    
-};
+export { userEmailValidation, valueLength };
