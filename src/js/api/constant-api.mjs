73846bd9e30aa749d6d09c67c4a6a35dbd8b90fe
Elise Marie Hogsnes <39590842+elli95@users.noreply.github.com>
2023-10-05 18:52:17 +0200

    Api url

--- src/js/api/constant-api.mjs
+++ src/js/api/constant-api.mjs
@@ -1,3 +1,5 @@
-const API_BASE_URL = "https://api.noroff.dev";
+const API_HOST_BASE_URL = "https://api.noroff.dev";
+const API_SOSIAL_BASE = "/api/v1/social";
+const API_SOSIAL_URL = `${API_HOST_BASE_URL}${API_SOSIAL_BASE}`;
 
-export { API_BASE_URL };
+export { API_SOSIAL_URL };
